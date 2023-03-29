357843bb9c12c62abf81b17af5c88ed43a830756
Anurag <anurag@anurag.bbrouter>
2023-03-29 20:22:56 +0530

    Updates

---
+++ precache-manifest.8c5ce85dd0734721e5120a51b0939bae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "984e7407f61b691c046e",
+    "url": "/edit/static/css/main.d569c4e4.chunk.css"
+  },
+  {
+    "revision": "984e7407f61b691c046e",
+    "url": "/edit/static/js/main.984e7407.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/edit/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "024959f85cf3a0edef0a",
+    "url": "/edit/static/js/2.024959f8.chunk.js"
+  },
+  {
+    "revision": "b3376a2c194f9e21eb44",
+    "url": "/edit/static/js/runtime~main.b3376a2c.js"
+  },
+  {
+    "revision": "90839a82abf153ff6bb445b247904bba",
+    "url": "/edit/index.html"
+  }
+];
